ed329ec6d87e1e4ab432fb816bf1e8a1fa7f6649
Dan Amzulescu <35110114+qualidan@users.noreply.github.com>
2021-08-12 14:48:13 -0500

    Update join-domain.sh

--- Bash/join-domain.sh
+++ Bash/join-domain.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo '$IP $AD_DOMAIN_NAME $AD_NETBIOS_NAME' >> /etc/resolv.conf
+echo '$AD_IP $AD_DOMAIN_NAME $AD_NETBIOS_NAME' >> /etc/resolv.conf
 realm join --user=username1 $AD_DOMAIN_NAME
