061bc0f82c7fbd9cd23893bbc1f7224b8bc73187
Dan Amzulescu <35110114+qualidan@users.noreply.github.com>
2021-08-13 13:36:16 -0500

    Update join-domain.sh

--- Bash/join-domain.sh
+++ Bash/join-domain.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
-sleep 5m
+sleep 6m
 
 sudo echo "$AD_IP $AD_NETBIOS_NAME.$AD_DOMAIN_NAME $AD_NETBIOS_NAME" >> /etc/hosts
 sudo echo "$AD_IP $AD_DOMAIN_NAME $AD_NETBIOS_NAME" >> /etc/hosts
-sudo realm join --user=Administrator $AD_NETBIOS_NAME
+echo $ADMIN_PASSWORD | realm join -U Administrator $AD_NETBIOS_NAME
