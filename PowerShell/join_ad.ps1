b388b285d9292c42039956759ff31d0ce43f8630
Dan Amzulescu <35110114+qualidan@users.noreply.github.com>
2021-08-13 11:52:34 -0500

    Update join_ad.ps1

--- PowerShell/join_ad.ps1
+++ PowerShell/join_ad.ps1
@@ -3,7 +3,7 @@ $defaultNic = Get-NetAdapter | Select ifIndex
 $defaultGW = Get-NetIPConfiguration -InterfaceIndex $defaultNic[0].ifIndex | Foreach IPv4DefaultGateway
 
 # Set DNS Servers
-Set-DNSClientServerAddress -interfaceIndex $defaultNic[0].ifIndex -ServerAddresses ($env:AD_IP,$defaultGW.NextHop)
+Set-DNSClientServerAddress -interfaceIndex $defaultNic[0].ifIndex -ServerAddresses $env:AD_IP
 
 Start-Sleep -Seconds 60
 
@@ -12,3 +12,5 @@ $password = $env:ADMIN_PASSWORD | ConvertTo-SecureString -asPlainText -Force
 $username = "$domain\Administrator"
 $credential = New-Object System.Management.Automation.PSCredential($username,$password)
 Add-Computer -DomainName $domain -Credential $credential
+shutdown -r -t 5
+exit 0
