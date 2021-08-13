e33d81a346fc2c7b30d6ac7ba2f4c52d5b8dbc49
Dan Amzulescu <35110114+qualidan@users.noreply.github.com>
2021-08-13 11:52:04 -0500

    Update install_ad.ps1

--- PowerShell/install_ad.ps1
+++ PowerShell/install_ad.ps1
@@ -2,5 +2,5 @@ Import-Module ADDSDeployment
 $password = ConvertTo-SecureString $env:ADMIN_PASSWORD -AsPlainText -Force
 Add-WindowsFeature -name ad-domain-services -IncludeManagementTools
 Install-ADDSForest -CreateDnsDelegation:$false -DomainMode Win2012R2 -DomainName $env:AD_DOMAIN_NAME -DomainNetbiosName $env:AD_NETBIOS_NAME -ForestMode Win2012R2 -InstallDns:$true -SafeModeAdministratorPassword $password -Force:$true
-shutdown -r -t 10
+shutdown -r -t 5
 exit 0
